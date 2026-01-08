badf4e5036da4ea3c9c84f1705339b97ae1b6c43
Thiago <tsmonteiro@gmail.com>
2026-01-08 12:42:22 -0300

    Update library versions

--- build.rs
+++ build.rs
@@ -1,9 +1,9 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Compile proto files using tonic-build
-    tonic_build::configure()
+    // Compile proto files using tonic-prost-build
+    tonic_prost_build::configure()
         .build_server(false) // Client only, no server code generation
         .build_transport(false) // Don't generate transport code (avoid naming conflicts)
-        .compile(
+        .compile_protos(
             &["protos/tercen.proto", "protos/tercen_model.proto"],
             &["protos"],
         )?;
